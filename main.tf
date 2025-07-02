6c0c5d294ac0792fdce786ca928d89795881bc92
Amit Jindal <AmitJindal@princenagaroutlook.onmicrosoft.com>
2025-07-02 10:59:09 +0530

    New rg added

--- main.tf
+++ main.tf
@@ -10,3 +10,11 @@ resource "azurerm_resource_group" "amit-rg" {
     location = each.value  # You can change this to your desired location.
   
 }
+
+
+resource "azurerm_resource_group" "amit-rg2" {
+    for_each = var.rg_names  # This will create resource groups with names like "kavi-rg0", "chavi-rg1", etc.  
+    name     = each.key  # This will create resource groups with names like "kavi-rg0", "chavi-rg1", etc.
+    location = each.value  # You can change this to your desired location.
+  
+}
